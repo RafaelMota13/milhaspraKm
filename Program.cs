ebbcf86976d508115793513db436c62272981b38
Rafael <faelmota7@gmail.com>
2022-09-17 09:55:28 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("\n--- Equivalência ---");
+Console.ResetColor();
+
+Console.Write("Entre com a medida (em milhas): ");
+double milhas = Convert.ToDouble(Console.ReadLine());
+
+
+double km = milhas * 1.609;
+
+Console.WriteLine($"{km:N3} Km");
